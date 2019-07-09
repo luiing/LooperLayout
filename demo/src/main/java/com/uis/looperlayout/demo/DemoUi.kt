a4717f94173ca58de05f7234dbdafe3708f84c57
uis <huaibing.liu@bl.com>
2019-07-09 13:36:51 +0800

    update demo

--- demo/src/main/java/com/uis/looperlayout/demo/DemoUi.kt
+++ demo/src/main/java/com/uis/looperlayout/demo/DemoUi.kt
@@ -1,22 +1,27 @@
 package com.uis.looperlayout.demo
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bailian.yike.find.view.LooperLayout
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.view.SimpleDraweeView
 import kotlinx.android.synthetic.main.item_looper2.view.*
 import kotlinx.android.synthetic.main.ui_demo.*
 import kotlin.random.Random
 
 class DemoUi :Activity(){
-
+    val images = arrayOf("https://www.baidu.com/img/bd_logo1.png",
+                "https://gss0.bdstatic.com/70cFfyinKgQIm2_p8IuM_a/daf/pic/item/e61190ef76c6a7efca21e788f3faaf51f3de661c.jpg",
+                "https://box.bdimg.com/static/fisp_static/common/img/searchbox/logo_news_276_88_1f9876a.png",
+                "https://box.bdimg.com/static/fisp_static/common/img/searchbox/logo_news_276_88_1f9876a.png")
     val random = Random(255)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Fresco.initialize(applicationContext)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ui_demo)
         looper1.refreshDataChange(arrayOf("胡先煦新恋情曝光新","祖峰新片退出戛纳新","杭州多名保安被捅","张铭恩接机徐璐新"))
@@ -33,10 +38,14 @@ class DemoUi :Activity(){
             }
 
             override fun onBindView(view: View, value: Any) {
-                view.v_color.setBackgroundColor(Color.rgb(random.nextInt(),random.nextInt(),random.nextInt()))
-                view.tv_content.text = value.toString()
+                view.v_color.let{it as SimpleDraweeView
+                    it.setImageURI(value.toString(),view.context)
+                    //Log.e("xx","value: "+value.toString())
+                }
+                //view.v_color.setBackgroundColor(Color.rgb(random.nextInt(),random.nextInt(),random.nextInt()))
+                //view.tv_content.text = value.toString()
             }
         })
-        looper2.refreshDataChange(arrayOf("相亲5次遇见爱情","大疆回应美国警告","男子被辞携妻跳楼","蓝汛CEO王松被捕"))
+        looper2.refreshDataChange(images)
     }
 }
